774528498137e4e6387d6132f178e2031efaacfd
Manuel Sousa <mlrcbsousa@gmail.com>
2021-11-02 21:51:02 +0100

    Include libft in the printf header file.

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 22:36:48 by msousa            #+#    #+#             */
-/*   Updated: 2021/11/01 15:13:29 by msousa           ###   ########.fr       */
+/*   Updated: 2021/11/02 21:50:19 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define FT_PRINTF_H
 
 # include <stdarg.h>
-// # include "libft.h"
+# include "libft.h"
 
 # define PRINTF_FORMATS "cspdiuxX%"
 # define PRINTF_DELIMITER '%'
