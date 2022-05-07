499aa6339abda6b41bf1b6103783525b0a02e13c
Manuel Sousa <mlrcbsousa@gmail.com>
2022-05-07 16:54:21 +0100

    Update get_next_line and remove const from ft_srtslen.

--- string/ft_strslen.c
+++ string/ft_strslen.c
@@ -6,14 +6,14 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 15:36:09 by msousa            #+#    #+#             */
-/*   Updated: 2022/05/07 15:49:15 by msousa           ###   ########.fr       */
+/*   Updated: 2022/05/07 16:51:19 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 // Let segfault when str is NULL so we know not to call funciton on NULL
-size_t	ft_strslen(const char **strs)
+size_t	ft_strslen(char **strs)
 {
 	size_t	length;
 
